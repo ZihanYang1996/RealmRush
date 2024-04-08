71b62ac607401514cda7e4af65193baf706d1ec9
Zihan Yang <ws704603301@gmail.com>
2024-04-08 15:07:08 -0400

    Make the enemy move smoothly, and facing right direction

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> waypoints = new List<Waypoint>();
+    [SerializeField] [Range(0.1f, 20f)] float moveSpeed = 1f;
     void Start()
     {
         StartCoroutine(FollowPath(waypoints));
@@ -13,15 +15,24 @@ public class EnemyMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Move(new Vector3(0, 0, 20), moveSpeed);
     }
 
     IEnumerator FollowPath(List<Waypoint> waypoints)
     {
         foreach (Waypoint waypoint in waypoints)
         {
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(1f);
+            transform.LookAt(waypoint.transform.position);
+            while (Vector3.Distance(transform.position, waypoint.transform.position) > Mathf.Epsilon)
+            {
+                Move(waypoint.transform.position, moveSpeed);
+                yield return new WaitForEndOfFrame();
+            }
         }
+    }
+
+    void Move(Vector3 position, float speed)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, position, speed * Time.deltaTime);
     }
 }
